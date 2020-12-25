3b9dbd36823d1d7a3863d2996d1042815033ff06
Atilla45 <atilla.abdullazade4503@gmail.com>
2020-12-26 00:33:14 +0400

    models

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,25 @@
 from django.db import models
-
+from accounts.models impor CustomUser as User
+from home.models import Category
 # Create your models here.
+class Skill(models.Model):
+    title=models.CharField('Title',max_length=150,)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+
 
 class Service(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     title=models.CharField('Title',max_length=150,)
     description=models.CharField('Description', max_length=500)
     category= models.ForeignKey(Category,on_delete=models.CASCADE)
+    image=models.FileField()
+    skill=models.ForeignKey(Skill,on_delete=models.CASCADE)
     is_published= models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
+
+    created_at = models.DateTimeField(auto_now_add=True,default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
         return self.title
@@ -17,8 +27,13 @@ class Service(models.Model):
 class Pocket(models.Model):
     title=models.CharField('Title',max_length=150,)
     description=models.CharField('Description', max_length=500)
-    service=models.ForeignKey(Service,on_delete=models.CASCADE)
+    service=models.ForeignKey(Service,on_delete=models.CASCADE,releated_name='pockets')
     cost=models.IntegerField(min_value=1, max_value=10000)
+
+    is_published = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+
     
     def __str__(self):
         return self.title
+    
