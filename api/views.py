e1617e9101460c597d72de7de6f37b7bcd6a109e
Atilla45 <atilla.abdullazade4503@gmail.com>
2020-12-27 00:52:00 +0400

    error

--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ from blog.models import Service
 from django.contrib.auth.models import Permission
 from rest_framework.permissions import IsAuthenticated
 from home.models import Category
-
+from rest_framework.response import Response
 # Create your views here.
 class ServiceView(generics.ListCreateAPIView):
     queyset=Service.objects.all()
@@ -22,11 +22,20 @@ class ServiceView(generics.ListCreateAPIView):
 
 
     def post(self,request):
-        serializer = ServiceSerializer(data=request.data, files=request.FILE)
+        serializer = ServiceSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(data=request.data)
 
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            category_id = self.request.GET['category'] 
+            skill_id=self.request.GET['skill']
+            self.queryset=self.queryset.filter(skill__id=skill_id)
+            self.queryset = self.queryset.filter(category__id=category_id)
+        except:
+            pass
+        return super(ServiceView, self).dispatch(request, *args, **kwargs)
 
 
 
